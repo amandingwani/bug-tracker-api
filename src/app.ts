eab09b6e18324cb318fcbec1f45207cb2d076fe2
Aman Dingwani <amandingwani@gmail.com>
2023-10-10 22:09:36 +0530

    refactor: Move app.listen to server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,8 @@
 import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
 import healthRoutes from './routes/health';
 
-dotenv.config();
-
 const app = express();
-const port = process.env.PORT;
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript Express!');
-});
 
 app.use('/health', healthRoutes);
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+export default app;
