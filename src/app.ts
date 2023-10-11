efbbcf44c9805eb619b1d2ae6127d76dd543df17
Aman Dingwani <amandingwani@gmail.com>
2023-10-12 00:49:41 +0530

    feat: Add cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 import healthRoutes from './routes/health';
 import authRoutes from './routes/auth';
 import middleware from './middleware';
@@ -6,6 +7,10 @@ import middleware from './middleware';
 
 const app = express();
 
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}));
 app.use(express.json());
 
 app.use('/health', healthRoutes);
