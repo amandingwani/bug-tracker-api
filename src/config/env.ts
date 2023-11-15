6248b5ff4abb81ec16e217dfb4b1c58504e0b11f
Aman Dingwani <amandingwani@gmail.com>
2023-11-15 09:01:37 +0530

    feat: Add handling for google code to token to userData

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,4 +6,5 @@ export const PORT = process.env.PORT || 3000;
 export const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:5173';
 export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const GOOGLE_OAUTH_CLIENT_ID = process.env.GOOGLE_OAUTH_CLIENT_ID;
+export const GOOGLE_OAUTH_CLIENT_SECRET = process.env.GOOGLE_OAUTH_CLIENT_SECRET;
 export const JWT_SECRET = process.env.JWT_SECRET || '!!PLEASE_GENERATE_A_NEW_JWT_SECRET!!';
--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,70 +1,55 @@
 import { Request, Response } from 'express';
-import { OAuth2Client } from 'google-auth-library';
 import jwt from 'jsonwebtoken';
 import prisma from '../db';
-import { GOOGLE_OAUTH_CLIENT_ID, JWT_SECRET } from '../config/env';
-
-const client = new OAuth2Client();
-
-async function verify(token: string) {
-	// https://developers.google.com/identity/openid-connect/openid-connect
-	const ticket = await client.verifyIdToken({
-		idToken: token,
-		audience: GOOGLE_OAUTH_CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-		// Or, if multiple clients access the backend:
-		//[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-	});
-	const payload = ticket.getPayload();
-	// console.log(payload);
-	// const userid = payload?.sub;
-	// // If request specified a G Suite domain:
-	// // const domain = payload['hd'];
-	return payload;
-}
+import { getTokens, verifyIdToken } from '../services/google'
+import { JWT_SECRET } from '../config/env';
 
 // login or register
 export const login = async (req: Request, res: Response) => {
 	try {
-		const payload = await verify(req.body.token);
-		if (payload) {
-			if (payload.email && payload.given_name) {
+		const tokens = await getTokens(req.body.code);
+		if (tokens.id_token) {
+			const payload = await verifyIdToken(tokens.id_token);
+			if (payload) {
+				if (payload.email && payload.given_name) {
 
-				// console.log(payload);
-				// if user already exits, just login, else register first then login
-				let user = await prisma.user.findUnique({
-					where: {
-						google_id_sub: payload.sub
-					}
-				});
-		
-				// login
-				if (user) {
-					console.log(user);
-					jwt.sign(user, JWT_SECRET, {} ,(err, token) => {
-						if (err) throw err;
-						res.cookie('token', token, {sameSite:'none', secure:true}).status(200).json({
-							status: 'ok'
-						});
-					});
-				}
-				// register
-				else {
-					user = await prisma.user.create({
-						data: {
-							google_id_sub: payload.sub,
-							email: payload.email,
-							firstName: payload.given_name,
-							lastName: payload.family_name,
-							picture: payload.picture
+					console.log(payload);
+					// if user already exits, just login, else register first then login
+					let user = await prisma.user.findUnique({
+						where: {
+							google_id_sub: payload.sub
 						}
 					});
-					console.log(user);
-					jwt.sign(user, JWT_SECRET, {} ,(err, token) => {
-						if (err) throw err;
-						res.cookie('token', token, {sameSite:'none', secure:true}).status(201).json({
-							status: 'ok'
+
+					// login
+					if (user) {
+						console.log(user);
+						jwt.sign(user, JWT_SECRET, {}, (err, token) => {
+							if (err) throw err;
+							res.cookie('token', token, { sameSite: 'none', secure: true }).status(200).json({
+								status: 'ok'
+							});
 						});
-					});
+					}
+					// register
+					else {
+						user = await prisma.user.create({
+							data: {
+								google_id_sub: payload.sub,
+								email: payload.email,
+								firstName: payload.given_name,
+								lastName: payload.family_name,
+								picture: payload.picture
+							}
+						});
+						console.log(user);
+						jwt.sign(user, JWT_SECRET, {}, (err, token) => {
+							if (err) throw err;
+							res.cookie('token', token, { sameSite: 'none', secure: true }).status(201).json({
+								status: 'ok'
+							});
+						});
+					}
 				}
 			}
 		}
