16c73c0bb3b5bb6e500b770c551c1eeec9bec9df
Aman Dingwani <amandingwani@gmail.com>
2023-11-16 14:22:16 +0530

    feat: Add logout

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { googleAuth } from '../controllers/googleAuth';
+import { logout } from '../controllers/logout';
 
 const router = express.Router();
 
 router.post('/google', googleAuth);
+router.post('/logout', logout);
 
 export default router;
