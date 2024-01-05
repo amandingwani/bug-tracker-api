bad131fac4aa67dbc973c745fb28315795cef46c
Aman Dingwani <amandingwani@gmail.com>
2024-01-05 15:42:52 +0530

    feat: googleAuth controller now handles unregistered users already in db

--- src/controllers/googleAuth.ts
+++ src/controllers/googleAuth.ts
@@ -12,21 +12,39 @@ export const googleAuth = async (req: Request, res: Response) => {
       const payload = await verifyIdToken(tokens.id_token);
       if (payload) {
         if (payload.email && payload.given_name) {
-          // if user already exits, just login, else register first then login
+          // checking for user in db
           let user = await prisma.user.findUnique({
             where: {
-              google_id_sub: payload.sub,
+              email: payload.email,
             },
           });
 
-          // login
+          // if user already exists
           if (user) {
+            // if user is unregistered, update the user
+            if (!user.registered) {
+              user = await prisma.user.update({
+                where: {
+                  email: payload.email,
+                },
+                data: {
+                  registered: true,
+                  google_id_sub: payload.sub,
+                  firstName: payload.given_name,
+                  lastName: payload.family_name,
+                  picture: payload.picture,
+                  createdAt: new Date()
+                },
+              })
+            }
+
+            // else, just login
             jwt.sign(user, JWT_SECRET, {}, (err, token) => {
               if (err) throw err;
               res.cookie('token', token, { sameSite: 'none', secure: true }).status(200).json(user);
             });
           }
-          // register
+          // else register first then login
           else {
             user = await prisma.user.create({
               data: {
@@ -47,7 +65,6 @@ export const googleAuth = async (req: Request, res: Response) => {
       }
     }
   } catch (error) {
-    res.json({});
-    console.log(error);
+    res.status(500).json({ error: error })
   }
 };
