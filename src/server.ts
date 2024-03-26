c56a056247ef62672149e64d6713b6e997f6c1cf
Aman Dingwani <amandingwani@gmail.com>
2024-03-26 21:11:51 +0530

    feat: Remove directUrl from prisma as pooler connection is not required

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import app from './app';
 import { PORT } from './config/env';
 
 app.listen(PORT, () => {
-  console.log(`${process.env.npm_package_name}\nServer running at http://localhost:${PORT}`);
+  console.log(`${process.env.npm_package_name}\nServer running on PORT:${PORT}`);
 });
